4531013eb286f04ae788ba5682547ad8b284315d
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-03-31 14:22:51 +0100

    fixed indentation

--- games/sqlmurdermystery.php
+++ games/sqlmurdermystery.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,23 +11,33 @@
             text-align: center;
             margin-top: 50px;
         }
+
         h1 {
             color: #333;
         }
+
         p {
             color: #555;
         }
     </style>
 </head>
+
 <body>
     <h1>SQL Murder Mystery</h1>
     <p>Solve the case</p>
 
-    <img src="images/ss.jpg" style="width: 750px;"
-    class="  display: block; margin-left: auto; margin-right: auto;">
+    <img src="images/ss.jpg" style="width: 750px;" class="  display: block; margin-left: auto; margin-right: auto;">
 
     <h2>There has been a serious murder is West City! and you have been hired to solve this crime
         We trust in you. </h2>
-        <p>use what you have learned about SQL to solve this crime, there are going to be help along side you.</p>
+    <p>use what you have learned about SQL to solve this crime, there are going to be help along side you.</p>
+
+
+
+
+
+
+
 </body>
+
 </html>
