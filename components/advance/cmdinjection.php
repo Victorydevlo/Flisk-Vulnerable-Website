3fef71274b43da8b4e2ed97aac2065194d83da56
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-04-02 12:37:27 +0100

    flag checker redirection

--- components/advance/cmdinjection.php
+++ components/advance/cmdinjection.php
@@ -191,7 +191,7 @@ include '../../userinfo/connection.php';
             result.textContent = 'Submitting your flag...';
             loadingMessage.style.display = 'inline';
 
-            fetch('filechecker/portsub.php', {
+            fetch('filechecker/cmdsub.php', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 body: `${flagName}=${encodeURIComponent(input.value.trim())}`
