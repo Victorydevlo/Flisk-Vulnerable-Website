4ccb52ec1ba56456e868f423adb4da38d906402f
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-04-06 19:04:23 +0100

    changed form chcker

--- components/complex/py.php
+++ components/complex/py.php
@@ -80,7 +80,7 @@ include '../../userinfo/connection.php';
             <h3>Get Working</h3>
         </div>
         <div class="task-content" id="content3">
-            <form id="flagForm" action="flagsub/cur.php" method="POST">
+            <form id="flagForm" action="flagsub/pysub.php" method="POST">
                 <div class="input-container">
                     <p><a href="../../images/hacks/download.php?file=reverse.py">Download</a></p>
                     <p>Submite the flag here</p>
@@ -110,7 +110,7 @@ include '../../userinfo/connection.php';
             result.textContent = 'Submitting your flag...';
             loadingMessage.style.display = 'inline';
 
-            fetch('flagsub/cur.php', {
+            fetch('flagsub/pysub.php', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 body: `${flagName}=${encodeURIComponent(input.value.trim())}`
