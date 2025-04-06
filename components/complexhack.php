3f92356e1e7dd715a404bad07824347095160121
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-04-06 18:58:21 +0100

    adding file redirection

--- components/complexhack.php
+++ components/complexhack.php
@@ -223,8 +223,7 @@ session_start();
             </div>
             <div style="position: relative; z-index: 2;">
                 <h3 style="margin-bottom: 10px; font-size: 1.2rem;">Docker Escape</h3>
-                <p style="font-size: 0.9rem; color: #e0e0e0;"><i>In this exercise you will learn how to access hidden pages
-                     in a website and prevent it</i>
+                <p style="font-size: 0.9rem; color: #e0e0e0;"><i></i>
                 </p>
                 <a href="simple/hiddenweb.php"
                     style="display: inline-block; padding: 5px 10px; margin-top: 10px; color: #fff; text-decoration: none; border: 2px solid transparent; border-image: linear-gradient(90deg, rgb(52, 52, 52), rgb(103, 103, 103), rgb(65, 65, 65)); border-radius: 20px; border-image-slice: 1;">Learn
@@ -242,8 +241,7 @@ session_start();
             </div>
             <div style="position: relative; z-index: 2;">
                 <h3 style="margin-bottom: 10px; font-size: 1.2rem;">Decrption made Hard</h3>
-                <p style="font-size: 0.9rem; color: #e0e0e0;"><i>In this exercise you will learn how to access hidden pages
-                     in a website and prevent it</i>
+                <p style="font-size: 0.9rem; color: #e0e0e0;"><i></i>
                 </p>
                 <a href="complex/decrypt.php"
                     style="display: inline-block; padding: 5px 10px; margin-top: 10px; color: #fff; text-decoration: none; border: 2px solid transparent; border-image: linear-gradient(90deg, rgb(52, 52, 52), rgb(103, 103, 103), rgb(65, 65, 65)); border-radius: 20px; border-image-slice: 1;">Learn
@@ -260,8 +258,7 @@ session_start();
             </div>
             <div style="position: relative; z-index: 2;">
                 <h3 style="margin-bottom: 10px; font-size: 1.2rem;">Currupted Image</h3>
-                <p style="font-size: 0.9rem; color: #e0e0e0;"><i>In this exercise you will learn how to access hidden pages
-                     in a website and prevent it</i>
+                <p style="font-size: 0.9rem; color: #e0e0e0;"><i></i>
                 </p>
                 <a href="complex/imagecurruption.php"
                     style="display: inline-block; padding: 5px 10px; margin-top: 10px; color: #fff; text-decoration: none; border: 2px solid transparent; border-image: linear-gradient(90deg, rgb(52, 52, 52), rgb(103, 103, 103), rgb(65, 65, 65)); border-radius: 20px; border-image-slice: 1;">Learn
@@ -278,8 +275,25 @@ session_start();
             </div>
             <div style="position: relative; z-index: 2;">
                 <h3 style="margin-bottom: 10px; font-size: 1.2rem;">64 eh?</h3>
-                <p style="font-size: 0.9rem; color: #e0e0e0;"><i>In this exercise you will learn how to access hidden pages
-                     in a website and prevent it</i>
+                <p style="font-size: 0.9rem; color: #e0e0e0;"><i></i>
+                </p>
+                <a href="complex/64eh.php"
+                    style="display: inline-block; padding: 5px 10px; margin-top: 10px; color: #fff; text-decoration: none; border: 2px solid transparent; border-image: linear-gradient(90deg, rgb(52, 52, 52), rgb(103, 103, 103), rgb(65, 65, 65)); border-radius: 20px; border-image-slice: 1;">Learn
+                    More</a>
+            </div>
+        </div>
+    </div>
+
+    
+    <div style="text-align: center; padding: 40px 20px;">
+        <div
+            style="position: relative; background: url('../images/lock3.jpg') center/cover; border-radius: 10px; padding: 20px; width: 300px; height: 200px; text-align: center; color: #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3); overflow: hidden;">
+            <div
+                style="content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 1; border-radius: 10px;">
+            </div>
+            <div style="position: relative; z-index: 2;">
+                <h3 style="margin-bottom: 10px; font-size: 1.2rem;">Reverse Me Pyton</h3>
+                <p style="font-size: 0.9rem; color: #e0e0e0;"><i>t</i>
                 </p>
                 <a href="complex/64eh.php"
                     style="display: inline-block; padding: 5px 10px; margin-top: 10px; color: #fff; text-decoration: none; border: 2px solid transparent; border-image: linear-gradient(90deg, rgb(52, 52, 52), rgb(103, 103, 103), rgb(65, 65, 65)); border-radius: 20px; border-image-slice: 1;">Learn
