68340e301a48cba4d7647376a2b3e1583ff6aadf
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-04-06 20:50:05 +0100

    added hint post

--- games/test/posts.php
+++ games/test/posts.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<br="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -141,18 +141,209 @@
         </div>
     </div>
 
-    <div class="instagram-post">
-        <div class="instagram-header">
-            <img src="https://i.pravatar.cc/40?img=10" alt="Profile Picture">
-            <span class="author">john</span>
-        </div>
-        <div class="instagram-content">
-            <p>“I absolutely love this site! It's such a fun way to learn about web vulnerabilities.”</p>
-        </div>
+<!-- Comment 1 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=5" alt="Profile Picture">
+    <span class="author">buzzQueen</span>
+  </div>
+  <div class="instagram-content">
+    <p>“Bees are essential to our ecosystem. Love this educational site!”</p>
+  </div>
+</div>
+
+<!-- Comment 2 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=8" alt="Profile Picture">
+    <span class="author">beekeeper42</span>
+  </div>
+  <div class="instagram-content">
+    <p>“Just spotted a carpenter bee outside my window!”</p>
+  </div>
+</div>
+
+<!-- Comment 3 + reply -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=10" alt="Profile Picture">
+    <span class="author">john</span>
+  </div>
+  <div class="instagram-content">
+    <p>“I absolutely love this site! It’s like honey for my brain.”</p>
+  </div>
+
+  <div class="reply-post">
+    <div class="instagram-header">
+      <img src="https://i.pravatar.cc/40?img=13" alt="Profile Picture">
+      <span class="author">honeyHunter</span>
+    </div>
+    <div class="instagram-content">
+      <p>“Haha! Sweet metaphor. Totally agree!”</p>
+    </div>
+  </div>
+</div>
+
+<!-- Comment 4 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=12" alt="Profile Picture">
+    <span class="author">J4son</span>
+  </div>
+  <div class="instagram-content">
+    <p>“i hacked the admin”</p>
+  </div>
+</div>
+
+<!-- Comment 5 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=15" alt="Profile Picture">
+    <span class="author">alex</span>
+  </div>
+  <div class="instagram-content">
+    <p>“Thank you for creating this amazing platform!”</p>
+  </div>
+
+  <div class="reply-post">
+    <div class="instagram-header">
+      <img src="https://i.pravatar.cc/40?img=20" alt="Profile Picture">
+      <span class="author">Owner</span>
+    </div>
+    <div class="instagram-content">
+      <p>“You are welcome!”</p>
     </div>
 
-    <footer>
-        <p>&copy; 2025 Vulnerability Playground</p>
-    </footer>
+    <div class="reply-post nested-reply">
+      <div class="instagram-header">
+        <img src="https://i.pravatar.cc/40?img=12" alt="Profile Picture">
+        <span class="author">J4son</span>
+      </div>
+      <div class="instagram-content">
+        <p>“No, it’s horrible.”</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Comment 6 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=25" alt="Profile Picture">
+    <span class="author">buzzlover</span>
+  </div>
+  <div class="instagram-content">
+    <p>“Did you know bees communicate by dancing?”</p>
+  </div>
+</div>
+
+<!-- Comment 7 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=22" alt="Profile Picture">
+    <span class="author">flowerpower</span>
+  </div>
+  <div class="instagram-content">
+    <p>“This platform is the bee's knees!”</p>
+  </div>
+</div>
+
+<!-- Comment 8 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=12" alt="Profile Picture">
+    <span class="author">J4son</span>
+  </div>
+  <div class="instagram-content">
+    <p>“ping me”</p>
+  </div>
+</div>
+
+<!-- Comment 9 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=18" alt="Profile Picture">
+    <span class="author">nectarNerd</span>
+  </div>
+  <div class="instagram-content">
+    <p>“Bees are like little flying scientists.”</p>
+  </div>
+</div>
+
+<!-- Comment 10 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=16" alt="Profile Picture">
+    <span class="author">beekarma</span>
+  </div>
+  <div class="instagram-content">
+    <p>“Save the bees, save the world.”</p>
+  </div>
+</div>
+
+<!-- Comment 11 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=12" alt="Profile Picture">
+    <span class="author">J4son</span>
+  </div>
+  <div class="instagram-content">
+    <p>“162”</p>
+  </div>
+</div>
+
+<!-- Comment 12 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=14" alt="Profile Picture">
+    <span class="author">buzzBook</span>
+  </div>
+  <div class="instagram-content">
+    <p>“I just finished a course on bee behavior here. Brilliant content!”</p>
+  </div>
+</div>
+
+<!-- Comment 13 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=19" alt="Profile Picture">
+    <span class="author">stingZone</span>
+  </div>
+  <div class="instagram-content">
+    <p>“When bees sting, they die. That's commitment.”</p>
+  </div>
+</div>
+
+<!-- Comment 14 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=9" alt="Profile Picture">
+    <span class="author">buzzbuzz</span>
+  </div>
+  <div class="instagram-content">
+    <p>“My favorite bug-themed site!”</p>
+  </div>
+</div>
+
+<!-- Comment 15 -->
+<div class="instagram-post">
+  <div class="instagram-header">
+    <img src="https://i.pravatar.cc/40?img=12" alt="Profile Picture">
+    <span class="author">J4son</span>
+  </div>
+  <div class="instagram-content">
+    <p>“the password is ........”</p>
+  </div>
+</div>
+
+
+
 </body>
+    </br>
+    </br>
+    </br>
+    </br>
+<footer>
+        <p>&copy; 2023 Bee Nation. All rights reserved.</p>
+    </footer>
 </html>
