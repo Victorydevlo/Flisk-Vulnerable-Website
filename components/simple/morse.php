d045509a636abbaf4baa0f24cf6f2eb7f4fb41a3
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-04-07 18:02:30 +0100

    new changes

--- components/simple/morse.php
+++ components/simple/morse.php
@@ -40,7 +40,7 @@ include '../../userinfo/connection.php';
     </div>
 
     <div class="header">
-        <div class="title">Binary</div>
+        <div class="title">Morse Code</div>
     </div>
     <div class="task">
         <div class="task-header" onclick="toggleContent('content1')">
